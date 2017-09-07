bfd7912158c432797f231fba5536630c408e7a26
Administrator <Administrator@Y8S0ZDNZUWOIWZV>
2017-09-07 08:45:36 +0800

    1

---
+++ third_ying/src/main/java/com/bean/Sms_record.java
@@ -0,0 +1,70 @@
+package com.bean;
+
+import java.util.Date;
+
+public class Sms_record {
+
+	private int id;
+	private int type;
+	private String phone;
+	private String code;
+	private String content;
+	private String result;
+	private int status;
+	private Date senddate;
+	private Date createdate;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getResult() {
+		return result;
+	}
+	public void setResult(String result) {
+		this.result = result;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public Date getSenddate() {
+		return senddate;
+	}
+	public void setSenddate(Date senddate) {
+		this.senddate = senddate;
+	}
+	public Date getCreatedate() {
+		return createdate;
+	}
+	public void setCreatedate(Date createdate) {
+		this.createdate = createdate;
+	}
+}
